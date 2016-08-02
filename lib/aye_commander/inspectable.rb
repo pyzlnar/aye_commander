6292063c336c4292e3d3a115df23ba7fa0aa15b1
JuanRamon@Monochrome <juan.ramon.fernandez.a@gmail.com>
2016-08-02 13:17:34 -0500

    Added Inspectable#to_hash
    This method will give a hash representation of what's inside the command
    (or result)
    It will be useful to pass info to another commands and or result class.

--- lib/aye_commander/inspectable.rb
+++ lib/aye_commander/inspectable.rb
@@ -4,10 +4,19 @@ module AyeCommander
     # This inspect mimics the one ActiveModel uses so hopefully it will also
     # look pretty during a pry session when the variables become too many.
     def inspect
-      inspection = instance_variables.map do |name|
-        "#{name}: #{instance_variable_get name}"
+      inspection = to_hash.map do |name, value|
+        "#{name}: #{value}"
       end.compact.join(', ')
       "#<#{self.class} #{inspection}>"
+    end
+
+    # Returns a hash of the specified instance_variables, default all
+    def to_hash(limit = instance_variables)
+      limit.reduce({}) do |hash, iv|
+        ivn = iv =~ /\A@/ ? iv : "@#{iv}".to_sym
+        hash[ivn] = instance_variable_get(ivn)
+        hash
+      end
     end
   end
 end
--- spec/aye_commander/inspectable_spec.rb
+++ spec/aye_commander/inspectable_spec.rb
@@ -2,11 +2,26 @@ describe AyeCommander::Inspectable do
   let(:command)  { Class.new.send(:include, AyeCommander::Command) }
   let(:instance) { command.new }
 
+  before :each do
+    instance.instance_variable_set :@variable, :something
+    instance.instance_variable_set :@other, :potato
+  end
+
   context '#inspect' do
-    it 'gives a string represntation of the innards of the class' do
-      instance.instance_variable_set :@variable, :something
-      instance.instance_variable_set :@other, :potato
+    it 'gives a string representation of the innards of the class' do
       expect(instance.inspect).to match /#<#<Class:\dx\w+> @status: success, @variable: something, @other: potato>/
+    end
+  end
+
+  context '#to_hash' do
+    it 'gives a hash representation of the innards of the class' do
+      result = { :@status => :success, :@variable => :something, :@other => :potato }
+      expect(instance.to_hash).to eq result
+    end
+
+    it 'gives a hash representation of the innards of the class with the requested values' do
+      result = { :@variable => :something, :@other => :potato }
+      expect(instance.to_hash([:@variable, :other])).to eq result
     end
   end
 end
